9938c10de30bbafbcb0bada00abb66e229ab7d87
Dennis <Dennisonung@gmail.com>
2022-05-20 09:57:04 -0400

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-axios.post('http://localhost:3000/create', {
-    url: 'https://ddc.gg/heeha/',
-}).then(function (response) {
-    console.log(response.data);
-    
-})
