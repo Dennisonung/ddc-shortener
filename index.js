deed372955219467a4c6dcb56a58e29762df54d7
Dennisonung <Dennisonung@gmail.com>
2022-10-03 01:29:32 -0400

    did some minor compacting

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const app = require('express')();
 const { existsSync, writeFileSync, readFileSync, mkdirSync } = require('fs');
-const bP = require('body-parser').json();
+app.use(require('body-parser').json());
 const sha = require('js-sha512').sha512;
-app.use(bP);
-function isURL(str) {
-    let res = str.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
-};
 function CreateShortenURL(url) {
     let link = url.slice(0, 4) === 'http' ? url : 'https://' + url;
-    let SavedURL = {url: link}
+    let SavedURL = {url: link};
     let sURL = sha(link).slice(0, 7);
     !existsSync('./ShortData/') && mkdirSync('./ShortData/');
-    if (existsSync(`./ShortData/${sURL}.json` && !JSON.parse(readFileSync(`./ShortData/${sURL}.json`, 'utf8')).sURL == sURL)) {
-            sURL = sha(link).slice(0, 8);
-    }
+    existsSync(`./ShortData/${sURL}.json`) && !JSON.parse(readFileSync(`./ShortData/${sURL}.json`, 'utf8')).sURL == sURL && (sURL = sha(link).slice(0, 8));
     writeFileSync(`./ShortData/${sURL}.json`, JSON.stringify(SavedURL), (err) => { console.log(err) });
-    //return "https://ddc.gg/t/" + sURL;
     return "http://localhost:8069/" + sURL;
 };
 app.get('/', (_req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 app.post('/create', (req, res) => {
-    isURL(req.body.url) ? res.send(CreateShortenURL(req.body.url)) : res.status(400).send("Invalid URL");
+    (req.body.url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) ? res.send(CreateShortenURL(req.body.url)) : res.status(400).send("Invalid URL");
 });
 app.get('/:sURL', (req, res) => {
     existsSync(`./ShortData/${req.params.sURL}.json`) ? res.redirect(JSON.parse(readFileSync(`./ShortData/${req.params.sURL}.json`, 'utf8')).url) : res.redirect("/");
