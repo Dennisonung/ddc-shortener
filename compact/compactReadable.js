95e8541d01824719909f0c1279d42e5b6a9c3913
Dennisonung <Dennisonung@gmail.com>
2023-12-28 01:59:05 -0500

    Update compactReadable.js

--- compact/compactReadable.js
+++ compact/compactReadable.js
@@ -1,9 +1,9 @@
-a=require;
-b=a('express');
-e=b();
-g=a('fs');
-e.use(b.json());
-e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"));
-e.post('/c',(r,s)=>{d=r.body.u;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)});
-e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)));
+a=require
+b=a('express')
+e=b()
+g=a('fs')
+e.use(b.json())
+e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"))
+e.post('/c',(r,s)=>{d=r.body.u;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)})
+e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)))
 e.listen(8069)
