cb259a0c80f63440a8deed931f270e495a87217f
Dennisonung <Dennisonung@gmail.com>
2023-12-28 01:57:52 -0500

    e

--- compact/compactReadable.js
+++ compact/compactReadable.js
@@ -1,10 +1,9 @@
 a=require;
 b=a('express');
 e=b();
-c=JSON;
 g=a('fs');
 e.use(b.json());
 e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"));
-e.post('/c',(r,s)=>{d=r.body.u;e=a('md5')(d);g.writeFileSync(e,`{"u":"${d}"}`);s.send(e)});
-e.get('/:e',(r,s)=>s.redirect(c.parse(g.readFileSync(r.params.e)).u));
+e.post('/c',(r,s)=>{d=r.body.u;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)});
+e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)));
 e.listen(8069)
