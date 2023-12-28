080bf857bc3b368a1be0bc5f06dc97b22fb8acfa
Dennisonung <Dennisonung@gmail.com>
2023-12-28 02:31:54 -0500

    woah even more compact

--- compact/compact.js
+++ compact/compact.js
@@ -1,1 +1,1 @@
-a=require;b=a('express');e=b();g=a('fs');e.use(b.json());e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"));e.post('/c',(r,s)=>{d=r.body;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)});e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)));e.listen(8069)
+a=require;b=a('express');e=b();e.use(b.text());g=a('fs');e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"));e.post('/c',(r,s)=>{d=r.body;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)});e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)));e.listen(8069)
--- compact/compactReadable.js
+++ compact/compactReadable.js
@@ -1,8 +1,8 @@
 a=require
 b=a('express')
 e=b()
+e.use(b.text())
 g=a('fs')
-e.use(b.json())
 e.get('',(r,s)=>s.sendFile(__dirname+"/i.htm"))
 e.post('/c',(r,s)=>{d=r.body;e=a('md5')(d);g.writeFileSync(e,d);s.send(e)})
 e.get('/:e',(r,s)=>s.redirect(g.readFileSync(r.params.e)))
