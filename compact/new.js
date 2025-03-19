45476eded3b46943d42fb562d728b851cd7e66f4
Dennisonung <Dennisonung@gmail.com>
2025-03-19 09:11:26 -0400

    Version 4

--- compact/new.js
+++
@@ -1,1 +0,0 @@
-a=require;b=a('express');b().use(b.text()).get('/',(r,s)=>s.sendFile(__dirname+'/i.htm')).post('/c',(r,s)=>{d=r.body;h=a('md5')(d);a('fs').writeFileSync(h,d);s.send(h)}).get('/:h',(r,s)=>s.redirect(a('fs').readFileSync(r.params.h))).listen(8069)
