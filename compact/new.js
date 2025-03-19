c2ccc8b5d0f32cc4bf59d4c248f2a4e260c542ed
Dennisonung <Dennisonung@gmail.com>
2025-03-19 08:55:45 -0400

    Create new.js

---
+++ compact/new.js
@@ -0,0 +1,1 @@
+a=require;a('express')().use(a('express').text()).get('/',(r,s)=>s.sendFile(__dirname+'/i.htm')).post('/c',(r,s)=>{d=r.body;h=a('md5')(d);a('fs').writeFileSync(h,d);s.send(h)}).get('/:h',(r,s)=>s.redirect(a('fs').readFileSync(r.params.h))).listen(8069)
